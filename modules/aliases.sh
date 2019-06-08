1f1d2861ee7de8940f6b6c5dcac27b702cb46e15
e-nen <eric@siskonen.com>
2019-06-07 23:12:59 -0400

    ill be back

---
+++ modules/aliases.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
